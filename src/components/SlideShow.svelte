e63ddcb8305b6247c8622fa662296886dca2915f
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-01 22:43:16 +0000

    slideshow.svelte?

--- src/components/SlideShow.svelte
+++ src/components/SlideShow.svelte
@@ -1,9 +1,36 @@
 <script lang="ts">
+  import { Image } from 'astro:assets';
+  import type { ImageMetadata } from 'astro';
+  export let images: ImageMetadata[] = [];
 
+  let currentIndex = 0;
+
+  const nextImage = () => {
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+
+  import { onMount, onDestroy } from 'svelte';
+  let interval;
+
+  onMount(() => {
+    interval = setInterval(nextImage, 3000);
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
 </script>
 
 <div>
-
+  {#each images as image, i}
+    <figure class:active={i === currentIndex}>
+      <Image 
+        src={image} 
+        alt="スライドショウの画像 {i + 1}" 
+        width={800}    height={500}   loading="eager"
+      />
+    </figure>
+  {/each}
 </div>
 
 <style lang="scss">
