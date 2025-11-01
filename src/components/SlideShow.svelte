86030c861c88aca1191518a6f319cbbf448bee37
Wing <114337707+wing-0902@users.noreply.github.com>
2025-11-01 22:55:54 +0000

    めっちゃOK

--- src/components/SlideShow.svelte
+++ src/components/SlideShow.svelte
@@ -1,40 +1,19 @@
-<script lang="ts">
-  import { Image } from 'astro:assets';
-  import type { ImageMetadata } from 'astro';
-  export let images: ImageMetadata[] = [];
+<script>
+  export let imagePaths = []; 
 
   let currentIndex = 0;
 
   const nextImage = () => {
-    currentIndex = (currentIndex + 1) % images.length;
-  }
+    currentIndex = (currentIndex + 1) % imagePaths.length;
+  };
 
-  import { onMount, onDestroy } from 'svelte';
-  let interval;
-
-  onMount(() => {
-    interval = setInterval(nextImage, 3000);
-  });
-
-  onDestroy(() => {
-    clearInterval(interval);
-  });
+  $: currentPath = imagePaths[currentIndex];
 </script>
 
-<div>
-  {#each images as image, i}
+<div class="slideshow-container">
+  {#each imagePaths as path, i}
     <figure class:active={i === currentIndex}>
-      <img
-        src={image} 
-        alt="スライドショウの画像 {i + 1}" 
-        width={800}
-        height={500}
-        loading="eager"
-      />
+      <img src={path} alt="スライドショウの画像 {i + 1}" />
     </figure>
   {/each}
 </div>
-
-<style lang="scss">
-
-</style>
