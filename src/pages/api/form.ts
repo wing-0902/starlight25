242ac084a62fb709de8b0adeed4f29def91dd037
tsubasa.terasawa0902 <tsubasa.terasawa0902@gmail.com>
2025-12-28 20:17:39 +0000

    RuntimeEnv types

--- src/pages/api/form.ts
+++ src/pages/api/form.ts
@@ -3,3 +3,8 @@ export const prerender = false;
 import { v4 as uuid } from 'uuid';
 import type { APIRoute } from 'astro';
 import type { KVNamespace } from '@cloudflare/workers-types';
+
+interface RuntimeEnv {
+  SURVEY_ANSWERS: KVNamespace;
+  TURNSTILE_SECRET_KEY: string;
+}
